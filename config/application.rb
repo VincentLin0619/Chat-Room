5ca51c99b7b2ced913f24ecc189065a83c967969
Zhe Kuan Lin <53500272+VincentLin0619@users.noreply.github.com>
2023-10-02 17:57:28 +0800

    安裝 gem devise devise-i18n 使用devise新稱 user model 並套用翻譯

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,9 +8,9 @@ Bundler.require(*Rails.groups)
 
 module ChatRoom
   class Application < Rails::Application
+    config.i18n.default_locale = 'zh-TW'
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "home_page#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
